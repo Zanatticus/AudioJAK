25ae4dd7e4113c8c70f83eb661a70fe75907b3bb
Zanatticus <zingare@live.com>
2024-03-17 17:22:07 -0400

    (#3) Modified visualizer init file to validate service working

--- visualization_server/visualizer.py
+++ visualization_server/visualizer.py
@@ -1,6 +1,10 @@
 import time
 from datetime import datetime
 
+path_to_file = "/home/zanatticus/prj-audiojak/visualization_server/visualizer.txt"
+
 while True:
-    print(datetime.now())
-    time.sleep(5)
+    with open(path_to_file, "a") as f:
+        f.write("The current timestamp is: " + str(datetime.now()) + "\n")
+        f.close()
+    time.sleep(10)
