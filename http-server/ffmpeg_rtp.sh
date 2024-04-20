e2690f15359d88e53c1e313d754db29b652c609f
Zanatticus <zingare@live.com>
2024-04-19 22:23:26 -0400

    #19 modifications

--- http-server/ffmpeg_rtp.sh
+++ http-server/ffmpeg_rtp.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
+RASPBERRY_PI_IP="129.10.158.202" # IP address of the Raspberry Pi. Could use localhost IP
 SOURCE="hw:CARD=3,DEV=0"
 AUDIO_OPTS="-re -f alsa -ac 2 -ar 48000 -sample_fmt s16"
 OUTPUT_OPTS="-acodec libmp3lame -f rtp"
-OUTPUT="rtp://127.0.0.1:1234"
+OUTPUT="rtp://$RASPBERRY_PI_IP:1234"
 ffmpeg $AUDIO_OPTS -i "$SOURCE" $OUTPUT_OPTS $OUTPUT
-
-# Essentially does the below command:
-# ffmpeg -re -f alsa -ac 2 -ar 48000 -sample_fmt s16 -i hw:CARD=3,DEV=0 -acodec libmp3lame -f rtp rtp://127.0.0.1:1234
 
 # This shell script is for streaming audio from the Raspberry Pi 
 # its localhost RTP port to verify that it can capture audio from the soundcard.
+
+# ffmpeg -re -f alsa -ac 2 -ar 48000 -sample_fmt s16 -i hw:CARD=3,DEV=0 -acodec libmp3lame -f rtp rtp://127.0.0.1:1234
--- http-server/ffmpeg_rtsp_client.sh
+++ http-server/ffmpeg_rtsp_client.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-VIDSOURCE="rtsp://audiojak:audiojak@129.10.158.202:554"
+RASPBERRY_PI_IP="129.10.158.202"
+VIDSOURCE="rtsp://audiojak:audiojak@$RASPBERRY_PI_IP:8554"
 AUDIO_OPTS="-c:a aac -b:a 160000 -ac 2"
 VIDEO_OPTS="-s 854x480 -c:v libx264 -b:v 800000"
-OUTPUT_HLS="-hls_time 10 -hls_list_size 10 -start_number 1"
+OUTPUT_HLS="-f hls -hls_time 1 -hls_list_size 15 -start_number 1 -hls_flags delete_segments"
 ffmpeg -i "$VIDSOURCE" -y $AUDIO_OPTS $VIDEO_OPTS $OUTPUT_HLS mystream.m3u8
 
 # This shell script is for streaming RTSP to HLS. It uses ffmpeg to convert 
