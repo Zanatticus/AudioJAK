4bb840549bb650e8eda63d9d9ba1c97393adbdc0
Zanatticus <zingare@live.com>
2024-04-19 23:49:09 -0400

    #19 test

--- http-server/ffmpeg_http.sh
+++ http-server/ffmpeg_http.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 SOURCE="hw:CARD=3,DEV=0"
 AUDIO_OPTS="-re -f alsa -ac 2 -ar 48000 -sample_fmt s16"
-OUTPUT_OPTS="-acodec libmp3lame -f hls -hls_time 2 -hls_list_size 15 -hls_segment_filename '/stream/audio%02d.ts' -hls_flags delete_segments"
+OUTPUT_OPTS="-acodec libmp3lame -f hls -hls_time 2 -hls_list_size 15 -hls_flags delete_segments"
 HLS_URL="http://localhost:8000/hls/stream.m3u8"
 
 ffmpeg $AUDIO_OPTS -i "$SOURCE" $OUTPUT_OPTS $HLS_URL
 
 # This shell script will capture audio and output it as an HLS playlist on a local HTTP server.
+# ffmpeg -re -f alsa -ac 2 -ar 48000 -sample_fmt s16 -i hw:CARD=3,DEV=0 -acodec libmp3lame -f hls -hls_time 2 -hls_list_size 15 -hls_segment_filename '/stream/audio%02d.ts' -hls_flags delete_segments http://localhost:8000/hls/stream.m3u8
