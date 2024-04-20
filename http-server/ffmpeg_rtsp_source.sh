9265a5a74bcbf52c9ba2d101562bedc5aba15943
Zanatticus <zingare@live.com>
2024-04-19 22:26:01 -0400

    #19 remove stream path

--- http-server/ffmpeg_rtsp_source.sh
+++ http-server/ffmpeg_rtsp_source.sh
@@ -3,7 +3,7 @@ RASPBERRY_PI_IP="129.10.158.202"
 SOURCE="hw:CARD=3,DEV=0"
 AUDIO_OPTS="-re -f alsa -ac 2 -ar 48000 -sample_fmt s16"
 OUTPUT_OPTS="-acodec libmp3lame -f rtsp"
-RTSP_URL="rtsp://$RASPBERRY_PI_IP:8554/stream"
+RTSP_URL="rtsp://$RASPBERRY_PI_IP:8554"
 
 ffmpeg $AUDIO_OPTS -i "$SOURCE" $OUTPUT_OPTS $RTSP_URL
 
