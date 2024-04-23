9f974612d84281ae834ca06997c52ff412e553cd
Zanatticus <zingare@live.com>
2024-04-23 14:35:28 -0400

    #19 removed unnecessary code? Fix for GET?

--- http-server/main.c
+++ http-server/main.c
@@ -22,7 +22,7 @@ static void signal_handler(int signo) {
 // Since not doing so is a security vulnerability :(
 static void set_cors_headers(struct mg_connection *c) {
     printf("Set_cors_headers: Pre-flight OPTIONS request received\n");
-    mg_http_reply(c, 204, "Content-Type: application/json\r\nAccess-Control-Allow-Origin: *\r\nAccess-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS\r\nAccess-Control-Allow-Headers: *\r\n\r\n",
+    mg_http_reply(c, 304, "Content-Type: application/json\r\nAccess-Control-Allow-Origin: *\r\nAccess-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS\r\nAccess-Control-Allow-Headers: *\r\n\r\n",
                          "No Content");
     // mg_http_reply(c, 204, "Access-Control-Allow-Origin: *\n"
     //                       "Access-Control-Allow-Methods: *\n"
@@ -39,50 +39,34 @@ static void cb(struct mg_connection *c, int ev, void *ev_data) {
   if (ev == MG_EV_HTTP_MSG) {
     struct mg_http_message *hm = ev_data;
 
-    // print the hm struct
-    // printf("hm->method: %.*s\n", (int)hm->method.len, hm->method.ptr);
-    // struct mg_str *s = mg_http_get_header(hm, "X-Extra-Header");
-    // printf("HEADER: %.*s\n", (int) s->len, s->ptr);
-
-    // //print just the hm struct:
-    // printf("hm: %.*s\n", (int)hm->message.len, hm->message.ptr);
-
-    // if (mg_vcmp(&hm->method, "OPTIONS") == 0) {
-    //       printf("Pre-flight OPTIONS request received\n");
-    
-    //       // Returns the Released CORS (ALL)
-    //       mg_http_reply(c, 204, "Content-Type: application/json\r\nAccess-Control-Allow-Origin: *\r\nAccess-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS\r\nAccess-Control-Allow-Headers: *\r\n\r\n",
-    //                     "No Content");
+    // if (mg_match(hm->uri, mg_str("/upload"), NULL)) {
+    //   printf("Upload request\n");
+    //   // Serve file upload
+    //   if (s_upload_dir == NULL) {
+    //     mg_http_reply(c, 403, "", "Denied: file upload directory not set\n");
+    //   } else {
+    //     struct mg_http_part part;
+    //     size_t pos = 0, total_bytes = 0, num_files = 0;
+    //     while ((pos = mg_http_next_multipart(hm->body, pos, &part)) > 0) {
+    //       char path[MG_PATH_MAX];
+    //       MG_INFO(("Chunk name: [%.*s] filename: [%.*s] length: %lu bytes",
+    //                part.name.len, part.name.ptr, part.filename.len,
+    //                part.filename.ptr, part.body.len));
+    //       mg_snprintf(path, sizeof(path), "%s/%.*s", s_upload_dir,
+    //                   part.filename.len, part.filename.ptr);
+    //       if (mg_path_is_sane(path)) {
+    //         mg_file_write(&mg_fs_posix, path, part.body.ptr, part.body.len);
+    //         total_bytes += part.body.len;
+    //         num_files++;
+    //       } else {
+    //         MG_ERROR(("Rejecting dangerous path %s", path));
+    //       }
+    //     }
+    //     mg_http_reply(c, 200, "", "Uploaded %lu files, %lu bytes\n", num_files,
+    //                   total_bytes);
+    //   }
     // } 
-
-    if (mg_match(hm->uri, mg_str("/upload"), NULL)) {
-      printf("Upload request\n");
-      // Serve file upload
-      if (s_upload_dir == NULL) {
-        mg_http_reply(c, 403, "", "Denied: file upload directory not set\n");
-      } else {
-        struct mg_http_part part;
-        size_t pos = 0, total_bytes = 0, num_files = 0;
-        while ((pos = mg_http_next_multipart(hm->body, pos, &part)) > 0) {
-          char path[MG_PATH_MAX];
-          MG_INFO(("Chunk name: [%.*s] filename: [%.*s] length: %lu bytes",
-                   part.name.len, part.name.ptr, part.filename.len,
-                   part.filename.ptr, part.body.len));
-          mg_snprintf(path, sizeof(path), "%s/%.*s", s_upload_dir,
-                      part.filename.len, part.filename.ptr);
-          if (mg_path_is_sane(path)) {
-            mg_file_write(&mg_fs_posix, path, part.body.ptr, part.body.len);
-            total_bytes += part.body.len;
-            num_files++;
-          } else {
-            MG_ERROR(("Rejecting dangerous path %s", path));
-          }
-        }
-        mg_http_reply(c, 200, "", "Uploaded %lu files, %lu bytes\n", num_files,
-                      total_bytes);
-      }
-    } 
-    else if (mg_http_match_uri(hm, "/hls/*")) {
+    if (mg_http_match_uri(hm, "/hls/*")) {
       printf("hm->uri: %.*s\n", (int)hm->uri.len, hm->uri.ptr);
       char file_path[256];
       snprintf(file_path, sizeof(file_path), "hls/%.*s", (int)hm->uri.len - 5, hm->uri.ptr + 5);
@@ -99,15 +83,22 @@ static void cb(struct mg_connection *c, int ev, void *ev_data) {
             mg_http_reply(c, 500, "", "Could not store file\n");
         }
       }
-      else {
-        // If GET request, respond with CORS headers
-        set_cors_headers(c);
-
-        // Serve web root directory
-        struct mg_http_serve_opts opts = {0};
-        opts.root_dir = s_root_dir;
-        opts.ssi_pattern = s_ssi_pattern;
-        mg_http_serve_dir(c, hm, &opts);
+      // If GET request do the following:
+      else if (mg_vcmp(&hm->method, "GET") == 0) {
+        FILE *fp = fopen(file_path, "rb");
+        if (fp != NULL) {
+            fseek(fp, 0, SEEK_END);
+            long file_size = ftell(fp);
+            fseek(fp, 0, SEEK_SET);
+            char *file_content = (char *)malloc(file_size);
+            fread(file_content, 1, file_size, fp);
+            fclose(fp);
+            mg_http_reply(c, 200, "", "%.*s", file_size, file_content);
+            free(file_content);
+        } 
+        else {
+            mg_http_reply(c, 404, "", "File not found\n");
+        }
       }
 
     }
